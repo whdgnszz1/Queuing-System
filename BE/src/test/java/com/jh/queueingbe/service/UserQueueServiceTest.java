16769b9e02b9883ed3e22e18a32c50684cad27e4
홍종훈 <whdgnszz1@snu.ac.kr>
2025-05-22 00:08:44 +0900

    feat: 대기열 허용, 허용여부 테스트코드 작성

--- BE/src/test/java/com/jh/queueingbe/service/UserQueueServiceTest.java
+++ BE/src/test/java/com/jh/queueingbe/service/UserQueueServiceTest.java
@@ -1,10 +1,13 @@
 package com.jh.queueingbe.service;
 
 import com.jh.queueingbe.EmbeddedRedis;
+import com.jh.queueingbe.exception.ApplicationException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.connection.ReactiveRedisConnection;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import reactor.test.StepVerifier;
@@ -22,6 +25,12 @@ class UserQueueServiceTest {
     @Autowired
     private ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
+    @BeforeEach
+    public void beforeEach() {
+        ReactiveRedisConnection redisConnection = reactiveRedisTemplate.getConnectionFactory().getReactiveConnection();
+        redisConnection.serverCommands().flushAll().subscribe();
+    }
+
     @Test
     void registerWaitQueue() {
         StepVerifier.create(userQueueService.registerWaitQueue("default", 100L))
@@ -38,10 +47,70 @@ class UserQueueServiceTest {
     }
 
     @Test
+    void alreadyRegisterWaitQueue() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L))
+                .expectNext(1L)
+                .verifyComplete();
+
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L))
+                .expectError(ApplicationException.class)
+                .verify();
+    }
+
+    @Test
     void allowUser() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L)
+                        .then(userQueueService.registerWaitQueue("default", 101L))
+                        .then(userQueueService.registerWaitQueue("default", 102L))
+                        .then(userQueueService.allowUser("default", 2L)))
+                .expectNext(2L)
+                .verifyComplete();
+    }
+
+    @Test
+    void allowUser2() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L)
+                        .then(userQueueService.registerWaitQueue("default", 101L))
+                        .then(userQueueService.registerWaitQueue("default", 102L))
+                        .then(userQueueService.allowUser("default", 5L)))
+                .expectNext(3L)
+                .verifyComplete();
+    }
+
+    @Test
+    void allowUserAfterRegisterWaitQueue() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L)
+                        .then(userQueueService.registerWaitQueue("default", 101L))
+                        .then(userQueueService.registerWaitQueue("default", 102L))
+                        .then(userQueueService.allowUser("default", 3L))
+                        .then(userQueueService.registerWaitQueue("default", 200L))
+                )
+                .expectNext(1L)
+                .verifyComplete();
+    }
+
+    @Test
+    void isNotAllowed() {
+        StepVerifier.create(userQueueService.isAllowed("default", 100L))
+                .expectNext(false)
+                .verifyComplete();
+    }
+
+    @Test
+    void isNotAllowed2() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L)
+                        .then(userQueueService.allowUser("default", 3L))
+                        .then(userQueueService.isAllowed("default", 101L)))
+                .expectNext(false)
+                .verifyComplete();
     }
 
     @Test
     void isAllowed() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L)
+                        .then(userQueueService.allowUser("default", 3L))
+                        .then(userQueueService.isAllowed("default", 100L)))
+                .expectNext(true)
+                .verifyComplete();
     }
 }
