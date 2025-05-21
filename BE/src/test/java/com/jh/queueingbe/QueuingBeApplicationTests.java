f05b69c23462994c53853f23c89e1db2e8419ab8
홍종훈 <whdgnszz1@snu.ac.kr>
2025-05-21 23:58:54 +0900

    feat: 유저 대기열 등록 테스트코드 작성

--- BE/src/test/java/com/jh/queueingbe/QueuingBeApplicationTests.java
+++ BE/src/test/java/com/jh/queueingbe/QueuingBeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class QueueingBeApplicationTests {
+class QueuingBeApplicationTests {
 
     @Test
     void contextLoads() {
---
+++ BE/src/test/java/com/jh/queueingbe/service/UserQueueServiceTest.java
@@ -0,0 +1,47 @@
+package com.jh.queueingbe.service;
+
+import com.jh.queueingbe.EmbeddedRedis;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Import(EmbeddedRedis.class)
+@ActiveProfiles("test")
+class UserQueueServiceTest {
+
+    @Autowired
+    private UserQueueService userQueueService;
+
+    @Autowired
+    private ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
+
+    @Test
+    void registerWaitQueue() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L))
+                .expectNext(1L)
+                .verifyComplete();
+
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 101L))
+                .expectNext(2L)
+                .verifyComplete();
+
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 102L))
+                .expectNext(3L)
+                .verifyComplete();
+    }
+
+    @Test
+    void allowUser() {
+    }
+
+    @Test
+    void isAllowed() {
+    }
+}
